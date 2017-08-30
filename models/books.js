f39f2b00322706a5eb6bdd20aad2ecf242ab23c3
gingin77 <ginniehench@gmail.com>
2017-08-29 20:37:35 -0400

    I've built the bookSchema model

---
+++ models/books.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    author: [{
+      last: { type: String },
+      first: { type: String }
+    }],
+    Category: {
+      type: String ,
+      enum: ["fiction", "non-fiction"]
+    },
+    deweyDecimalClass: { type: String },
+    deweyDecimalNumber: { type: Number },
+    genre: { type: String},
+    physicalProps: [{
+      pages: { type: Number },
+      cover: {
+        type: String,
+        enum: ["hardcover", "paperback", "other"]
+      },
+      spineWidth: { type: Number },
+      height: { type: Number },
+      coverAppearance: [{
+        color: { type: String },
+        image: { type: String }
+      }]
+    }]
+})
+
+const Books = mongoose.model('Books', bookSchema);
+
+module.exports = Books;
